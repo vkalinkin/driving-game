979c6c4c848af3aa46cc7736c116c99d5ce66c61
Victor Kalinkin <vakalinkin@hotmail.com>
2021-06-03 13:12:12 -0700

    completed issue 1, car visible, no OOP used

---
+++ main.js
@@ -0,0 +1,11 @@
+// var $raceTrack = document.querySelector('.race-track');
+
+// function Car(imgUrl){
+//   this.imgUrl = imgUrl;
+// }
+
+// var myCar = new Car("images/f1.svg");
+// var carImg = document.createElement('img');
+// carImg.setAttribute('src', myCar.imgUrl);
+
+// $raceTrack.appendChild(carImg);
