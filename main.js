0fab6af0b824a69dffa4d6147b50e1eb1b737eaa
Victor Kalinkin <vakalinkin@hotmail.com>
2021-06-03 15:21:52 -0700

    6 car only moves after spacekey is pressed

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 // var $car = document.querySelector('.car');
 
+// var moveBoolean = false;
+
 // var x = 0;
 // var y = 0;
 
 // var moveInterval = setInterval(moveCar, 16);
 
 // function moveCar(event){
-//   x += 2;
-//   $car.style.left = x + 'px';
+//   if (moveBoolean === true){
+//     x += 2;
+//     $car.style.left = x + 'px';
+//   }
 // }
+
+// document.addEventListener('keydown', function(event){
+//   if (`${event.code}` === 'Space'){
+//     moveBoolean = true;
+//   }
+// });
