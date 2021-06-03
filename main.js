7df4c38975f6038e942e8544aa66fcb3321b8b76
Victor Kalinkin <vakalinkin@hotmail.com>
2021-06-03 16:02:14 -0700

    3 car image changes direction based on direction var

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var $car = document.querySelector('.car');
 
 var moveBoolean = false;
 
-// var direction = 'e'
+var direction = 'e';
 var x = 0;
 // var y = 0;
 
@@ -16,17 +16,29 @@ function moveCar(event) {
   }
 }
 
-// document.addEventListener('keydown', function (event) {
-//   if (`${event.code}` === 'Space') {
-//     moveBoolean = true;
-//   } else if (`${event.code}` === 'ArrowRight'){
-//     direction = 'e';
-//   } else if (`${event.code}` === 'ArrowLeft'){
-//     direction = 'w';
-//   } else if (`${event.code}` === 'ArrowUp'){
-//     direction = 'n';
-//   } else if (`${event.code}` === 'ArrowDown'){
-//     direction = 's';
-//   }
-//   // console.log('direction:', direction);
-// });
+function rotateCar() {
+  if (direction === 's') {
+    $car.style.transform = 'rotate(90deg)';
+  } else if (direction === 'w') {
+    $car.style.transform = 'rotate(180deg)';
+  } else if (direction === 'n') {
+    $car.style.transform = 'rotate(270deg)';
+  } else if (direction === 'e') {
+    $car.style.transform = 'rotate(0deg)';
+  }
+}
+
+document.addEventListener('keydown', function (event) {
+  if (`${event.code}` === 'Space') {
+    moveBoolean = true;
+  } else if (`${event.code}` === 'ArrowRight') {
+    direction = 'e';
+  } else if (`${event.code}` === 'ArrowLeft') {
+    direction = 'w';
+  } else if (`${event.code}` === 'ArrowUp') {
+    direction = 'n';
+  } else if (`${event.code}` === 'ArrowDown') {
+    direction = 's';
+  }
+  rotateCar();
+});
