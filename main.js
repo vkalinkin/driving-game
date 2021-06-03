c04098560ca5216733311b32113720442f940f1d
Victor Kalinkin <vakalinkin@hotmail.com>
2021-06-03 15:30:59 -0700

    resubmission

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-// var $car = document.querySelector('.car');
+var $car = document.querySelector('.car');
 
-// var moveBoolean = false;
+var moveBoolean = false;
 
-// var x = 0;
+var x = 0;
 // var y = 0;
 
 // var moveInterval = setInterval(moveCar, 16);
+setInterval(moveCar, 16);
 
-// function moveCar(event){
-//   if (moveBoolean === true){
-//     x += 2;
-//     $car.style.left = x + 'px';
-//   }
-// }
+function moveCar(event) {
+  if (moveBoolean === true) {
+    x += 2;
+    $car.style.left = x + 'px';
+  }
+}
 
-// document.addEventListener('keydown', function(event){
-//   if (`${event.code}` === 'Space'){
-//     moveBoolean = true;
-//   }
-// });
+document.addEventListener('keydown', function (event) {
+  if (`${event.code}` === 'Space') {
+    moveBoolean = true;
+  }
+});
